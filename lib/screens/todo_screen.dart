8e1ed03a48e2012f039215848043f95ab40825c0
Octaviofg <48572130+OctavioFloG@users.noreply.github.com>
2025-02-21 16:31:20 -0600

    Screen todo_screen
    -Inicio de la pantalla para mostrar los TodoModel

--- lib/screens/todo_screen.dart
+++ lib/screens/todo_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/database/task_databa.dart';
 
 class TodoScreen extends StatefulWidget {
   const TodoScreen({super.key});
@@ -8,8 +9,36 @@ class TodoScreen extends StatefulWidget {
 }
 
 class _TodoScreenState extends State<TodoScreen> {
+  TaskDatabase? database;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    database = TaskDatabase();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("TodoList"),
+      ),
+      body: FutureBuilder(
+          future: database!.SELECTALL(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Center(
+                child: Text("Algo ha ocurrido"),
+              );
+            }
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemBuilder: (context, index) {},
+              );
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          }),
+    );
   }
 }
