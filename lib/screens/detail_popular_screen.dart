3fab09a55609222d8bf7ab8d1e92a620c2691c44
Octaviofg <48572130+OctavioFloG@users.noreply.github.com>
2025-04-02 16:27:38 -0600

    Prueba de libreria de video y vote Average

--- lib/screens/detail_popular_screen.dart
+++ lib/screens/detail_popular_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/models/popular_model.dart';
+import 'package:flutter_rating/flutter_rating.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 class DetailPopularScreen extends StatefulWidget {
   DetailPopularScreen({super.key, this.popularModel});
@@ -11,6 +13,15 @@ class DetailPopularScreen extends StatefulWidget {
 }
 
 class _DetailPopularScreenState extends State<DetailPopularScreen> {
+  YoutubePlayerController? _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = YoutubePlayerController.fromVideoId(
+        videoId: "xvFZjo5PgG0", autoPlay: true);
+  }
+
   @override
   Widget build(BuildContext context) {
     final popular = ModalRoute.of(context)!.settings.arguments as PopularModel;
@@ -20,35 +31,67 @@ class _DetailPopularScreenState extends State<DetailPopularScreen> {
       appBar: AppBar(
         title: Text(popular.title),
       ),
-      body: Hero(
-        tag: "https://image.tmdb.org/t/p/w500${popular.posterPath}",
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  opacity: .5,
-                  image: NetworkImage(
-                      "https://image.tmdb.org/t/p/w500${popular.posterPath}"))),
-          child: ListView(
-            shrinkWrap: true,
-            children: [
-              Container(
-                height: 250,
-                color: Colors.red,
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            gradient: const LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+              Colors.white,
+              Colors.blue,
+            ]
+                // image: DecorationImage(
+                //     opacity: .5,
+                //     image: NetworkImage(
+                //         "https://image.tmdb.org/t/p/w500${popular.posterPath}")
+
+                )),
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+            SizedBox(
+              height: 230,
+              child: YoutubePlayer(
+                controller: _controller!,
+                aspectRatio: 16 / 9,
               ),
-              space,
-              Container(
-                height: 200,
-                color: Colors.white,
-                child: Text(popular.overview),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              height: 250,
+              child: Row(
+                spacing: 10,
+                children: [
+                  Hero(
+                    tag: "https://image.tmdb.org/t/p/w500${popular.posterPath}",
+                    child: Image.network(
+                        "https://image.tmdb.org/t/p/w500${popular.posterPath}"),
+                  ),
+                  SizedBox(
+                      width: MediaQuery.of(context).size.width * .55,
+                      child: Text(
+                        popular.overview,
+                        textAlign: TextAlign.justify,
+                      ))
+                ],
               ),
-              space,
-              Container(
-                height: 100,
-                color: Colors.blue,
-              )
-            ],
-          ),
+            ),
+            space,
+            Container(
+              height: 200,
+              color: Colors.white,
+              child: Text(popular.overview),
+            ),
+            space,
+            Container(
+              height: 100,
+              child: Column(children: [
+                StarRating(rating: popular.voteAverage / 2),
+                Text((popular.voteAverage / 2).toString())
+              ]),
+            ),
+          ],
         ),
       ),
     );
