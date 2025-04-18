f82910a20ac59bc0fc3adafa71db9fc7b65fd337
Octaviofg <48572130+OctavioFloG@users.noreply.github.com>
2025-04-18 10:16:20 -0600

    Update producto_model.dart
    - unnecessary dateTodo field in model producto

--- lib/models/producto_model.dart
+++ lib/models/producto_model.dart
@@ -2,17 +2,15 @@ class ProductoModel {
   int? idProducto;
   String? nombre;
   int? precio;
-  String? dateTodo;
   int? stock;
 
-  ProductoModel({this.idProducto, this.nombre, this.precio, this.dateTodo, this.stock});
+  ProductoModel({this.idProducto, this.nombre, this.precio, this.stock});
 
   factory ProductoModel.fromMap(Map<String, dynamic> map) {
     return ProductoModel(
       idProducto: map['idProducto'],
       nombre: map['nombre'],
       precio: map['precio'],
-      dateTodo: map['dateTodo'],
       stock: map['stock'],
     );
   }
